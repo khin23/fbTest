2fd1e7b595b3b2beddd6d9797f9a5b2faaaaea02
khin <khin@DESKTOP-FNJ3SU3>
2017-09-11 18:09:00 +0800

    Add project files.

---
+++ fbTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fbTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
